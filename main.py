1469390184ce99e8ef06de856c431c5cef24d1f2
mark <kerothefrogdude@gmail.com>
2022-07-07 10:26:56 -0700

    got the list of champions and their screenshots

--- main.py
+++ main.py
@@ -4,12 +4,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import os
 from selenium import webdriver
-#import undetected_chromedriver as webdriver
+from riotwatcher import LolWatcher, ApiError
+from pprint import pprint
 #like the selenium web driver, but is better at detecting adds
 os.environ["PATH"] += r"C:\Selenium Drivers\chromedriver_win32"
 WINDOWSIZE = "1080,1080"
 SCROLLSIZE = ".05"
-
+API_KEY = r"RGAPI-442e7a6b-3dc2-4500-8b8a-4abe070cdc22"
 def create_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('disable-notifications')
@@ -20,23 +21,26 @@ def create_chrome_driver():
     return driver
 
 def create_aram_snapshot(driver, champ_name):
+    print(champ_name)
     driver.get(fr"https://app.mobalytics.gg/lol/champions/{champ_name}/aram-builds")
     driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight * {SCROLLSIZE})")
     driver.save_screenshot(fr"C:\Users\overl\PycharmProjects\AramSnapshot\champ_snapshots\{champ_name}.png")
-    driver.close()
+
+def update_snapshots(driver):
+    lol_watcher = LolWatcher(API_KEY)
+    my_region = 'na1'
+    versions = lol_watcher.data_dragon.versions_for_region(my_region)
+    champions_version = versions['n']['champion']
+    current_champ_list = lol_watcher.data_dragon.champions(champions_version)
+
+    current_champ_list = [champ.lower() for champ in current_champ_list["data"]]
+    for champ in current_champ_list:
+        create_aram_snapshot(driver, champ)
 
 if __name__ == '__main__':
-    #SET UP CHROME ENVIRNOMENT
     driver = create_chrome_driver()
-    create_aram_snapshot(driver, "ashe")
-    # Disables popups in chrome
 
-
-
-    # #increases load speed
-    # options = Options()
-    # options.page_load_strategy = 'none'
-
+    driver.quit()
 
 
 
